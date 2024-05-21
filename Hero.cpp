23b8b06ccb48c774df770b12dace77dfe4109121
kazuhiroo <ap.katowski@gmail.com>
2024-05-21 21:50:32 +0100

    Hero movement variables

--- Hero.cpp
+++ Hero.cpp
@@ -4,3 +4,7 @@ Hero::Hero(sf::Texture t, sf::Vector2f p): AnimatedObject(t,p){
 	this->setTexture(texture);
 	this->setPosition(position);
 }
+
+Hero::~Hero() {
+
+}
--- Hero.h
+++ Hero.h
@@ -1,11 +1,28 @@
 #pragma once
 #include "include.h"
+
+enum class Action {
+	stable,
+	in_air
+};
+
 class Hero: public AnimatedObject
 {
 private:
 
+	//movement variables
+	float jump_height = 100.0;
+	float velocity_jump = 0;
+
+	const float velocity_vertical = 200.0;
+	const float g = 981.0;
+
+	//state
+	Action state = Action::stable;
 
 public:
+	//contructors
 	Hero(sf::Texture t, sf::Vector2f p);
+	~Hero();
 };
 
